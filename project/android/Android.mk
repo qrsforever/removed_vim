e85e7a391ea52ea90b9553a0358c8029f3678a9b
qrsforever <lidong8@le.com>
2017-03-28 16:52:55 +0800

    1. add android android.mk模板以及混淆配置 2. 修改YCM的快捷键

---
+++ project/android/Android.mk
@@ -0,0 +1,50 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+src_dirs : src
+res_dirs : res
+aidl_dirs: java
+
+# 选择什么情况下编译 user, eng, tests, optional(所有版本)
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under, $(src_dirs)) 
+
+LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dirs))
+			
+# 指定AIDL的路径
+LOCAL_AIDL_INCLUDES := 
+
+# 依赖的共享库
+LOCAL_JAVA_LIBRARIES :=
+
+# 依赖的静态库
+LOCAL_STATIC_JAVA_LIBRARIES :=
+
+
+LOCAL_PACKAGE_NAME :=
+
+# 签名类型testkey, platform, shared, media
+LOCAL_CERTIFICATE := platform
+
+# 使能混淆
+LOCAL_PROGUARD_ENABLED := full
+
+# 指定混淆配置
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+# 指定overlay的相对路径 :overlay/${PRODUCT_NAME}的路径要和src_dirs一致
+# LOCAL_PACKAGE_OVERLAYS := overlay/${PRODUCT_NAME}
+
+LOCAL_DEX_PREOPT := false
+
+# 不是标准的java库
+# LOCAL_NO_STANDARD_LIBRARIES := true
+
+include $(BUILD_PACKAGE)
+
+# include $(BUILD_JAVA_LIBRARY)
+
+# Use the folloing include to make our test apk.
+include $(call all-makefiles-under,$(LOCAL_PATH))
